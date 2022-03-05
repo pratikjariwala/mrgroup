e60cae95e50bb2000ac0bddf94a2428f2209476a
Pratik <pratikjariwala6696@gmail.com>
2022-03-05 20:08:27 +0530

    2022-03-05--Registration api added

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const dot = require("dotenv");
+dot.config();
+const connectDB = require("./config/db");
 
 const app = express();
 
+// Connect Database
+connectDB();
+
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API running!"));
+
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 const PORT = process.env.PORT || 5000;
 
